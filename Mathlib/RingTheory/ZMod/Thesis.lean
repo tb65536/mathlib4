a25ed5cfe9fe46ab3cbc97e513fe4776185a441e
Thomas Browning <tb65536@uw.edu>
2025-07-22 18:39:44 -0700

    more skeleton

--- Mathlib/RingTheory/ZMod/Thesis.lean
+++ Mathlib/RingTheory/ZMod/Thesis.lean
@@ -12,8 +12,9 @@ open Pointwise
 
 theorem lem (G X : Type*) [Group G] [MulAction G X] [TopologicalSpace X]
     [SecondCountableTopology X] [MeasurableSpace X] [BorelSpace X]
-    (χ : G →* ℝ≥0) -- downgrade to function?
+    (χ : G → ℝ≥0)
     (C : Set (Set X))
+    (hS0 : ∃ S0 ∈ C, Nonempty S0) -- If no such S0 exists, then topology on X is trivial by hC5
     (hC1 : ∀ g : G, ∀ S ∈ C, g • S ∈ C)
     (hC2: MeasureTheory.IsSetRing C)
     (hC3 : ∀ S ∈ C, IsCompact S)
@@ -25,31 +26,36 @@ theorem lem (G X : Type*) [Group G] [MulAction G X] [TopologicalSpace X]
       μ A T = χ g * μ A S)
     (hμ3 : ∀ A : Finset C, ∀ S T U : A, Disjoint (S : Set X) (T : Set X) →
       (S : Set X) ∪ (T : Set X) = (U : Set X) → μ A U = μ A S + μ A T) :
-    ∃ μ : MeasureTheory.Measure X, μ ≠ 0 ∧ ∀ g : G, ∀ S : Set X, μ (g • S) = χ g * μ S := by -- Exists nonzero G-invariant Radon measure on X
-  have hS0 : ∃ S0 ∈ C, Nonempty S0 := by
-    simp only [Set.nonempty_coe_sort]
-    by_contra! h
-    rw [← Set.subset_singleton_iff] at h
-    replace h := MeasurableSpace.generateFrom_mono h
-    rw [hC5, MeasurableSpace.generateFrom_singleton_empty] at h
-    -- should imply that the topology is trivial on X, in which theorem is trivial
-    sorry
+    -- todo: add regularity condition
+    ∃ μ : MeasureTheory.Measure X, μ ≠ 0 ∧ ∀ g : G, ∀ S : Set X, μ (g • S) = χ g * μ S := by
   obtain ⟨S0, hS0C, hS0⟩ := hS0
   let σ : Finset C → Set X → NNReal :=
     fun A S ↦ Function.extend (↑) (μ A) (0 : Set X → NNReal) S
-  let τ : Finset C → Set X → NNReal :=
+  let τ : Finset C → Set X → ENNReal :=
     fun A S ↦ (σ A S0)⁻¹ * σ A S
   have hinj {A : Finset C} : Function.Injective ((↑) : A → Set X) := by
     intro x y h
     ext1
     ext1
     exact h
+  have hτ0 : ∀ A : Finset C, τ A ∅ = 0 := by
+    intro A
+    dsimp only [τ, σ]
+    by_cases h : ∃ S : A, S = (∅ : Set X)
+    · obtain ⟨S, hS⟩ := h
+      rw [← hS, hinj.extend_apply]
+      have key := hμ3 A S S S ?_ ?_
+      · rw [left_eq_add] at key
+        rw [key, ENNReal.coe_zero, mul_zero]
+      · rw [hS, disjoint_self, Set.bot_eq_empty]
+      · rw [Set.union_self]
+    · rw [Function.extend_apply' _ _ _ h, Pi.zero_apply, ENNReal.coe_zero, mul_zero]
   have hτ1 : ∀ A : Finset C, ⟨S0, hS0C⟩ ∈ A → τ A S0 = 1 := by
     intro A hS0A
     dsimp only [τ, σ]
     have h : ∃ T0 : A, T0 = S0 := ⟨⟨⟨S0, hS0C⟩, hS0A⟩, rfl⟩
     obtain ⟨T0, rfl⟩ := h
-    rw [hinj.extend_apply, inv_mul_cancel₀]
+    rw [hinj.extend_apply, ← ENNReal.coe_mul, inv_mul_cancel₀, ENNReal.coe_one]
     exact (hμ1 A T0 hS0).ne'
   have hτ2 : ∀ A : Finset C, ∀ g : G, ∀ S : Set X, (hSC : S ∈ C) → ⟨S, hSC⟩ ∈ A →
       ⟨g • S, hC1 g S hSC⟩ ∈ A → τ A (g • S) = χ g * τ A S := by
@@ -63,7 +69,8 @@ theorem lem (G X : Type*) [Group G] [MulAction G X] [TopologicalSpace X]
       rw [hgT]
       have h : ∃ T : A, T = S := ⟨⟨⟨S, hSC⟩, hSA⟩, rfl⟩
       obtain ⟨T, rfl⟩ := h
-      rw [hinj.extend_apply, hinj.extend_apply, hμ2 A g T gT hgT]
+      rw [hinj.extend_apply, hinj.extend_apply, hμ2 A g T gT hgT, ← ENNReal.coe_mul,
+        ← ENNReal.coe_mul, ← ENNReal.coe_mul, ENNReal.coe_inj]
       group
     · simp [Function.extend_apply' _ _ _ h]
   have hτ3 : ∀ A : Finset C, ∀ S T : Set X, Disjoint S T → (hSC : S ∈ C) → (hTC : T ∈ C) →
@@ -82,12 +89,36 @@ theorem lem (G X : Type*) [Group G] [MulAction G X] [TopologicalSpace X]
       have h : ∃ T' : A, T' = T := ⟨⟨⟨T, hTC⟩, hTA⟩, rfl⟩
       obtain ⟨T', rfl⟩ := h
       rw [hinj.extend_apply, hinj.extend_apply, hinj.extend_apply,
-        hμ3 A S' T' ST' hST hST', mul_add]
+        hμ3 A S' T' ST' hST hST', ← ENNReal.coe_mul, mul_add, ENNReal.coe_add,
+        ENNReal.coe_mul, ENNReal.coe_mul]
     · simp [Function.extend_apply' _ _ _ h]
-
-
-  -- have hτ3 : ∀ A : Set (Set X), (hA : A.Finite) → (hAC : A ⊆ C) → ∀ S T : Set X,
-  --   Disjoint S T → (hSA : S ∈ A) → (hTA : T ∈ A) → (hSTA : S ∪ T ∈ A) →
-  --     μ A hA hAC ⟨S ∪ T, hSTA⟩ = μ A hA hAC ⟨S, hSA⟩ + μ A hA hAC ⟨T, hTA⟩ := by
-  --   sorry
-  sorry
+  have h : CompactSpace (Set X → ENNReal) := by
+    infer_instance
+  obtain ⟨μ, -, hμ⟩ := h.isCompact_univ.exists_mapClusterPt (f := Filter.atTop) (u := τ) (by simp)
+  replace hμ0 : μ ∅ = 0 := by
+    sorry
+  replace hμ1 : μ S0 = 1 := by
+    rw [mapClusterPt_iff_frequently] at hμ
+    sorry
+  replace hμ2 : ∀ g : G, ∀ S : Set X, S ∈ C → μ (g • S) = χ g * μ S := by
+    intro g S hS
+    sorry
+  replace hμ3 : ∀ {S T : Set X}, S ∈ C → T ∈ C → Disjoint S T → μ (S ∪ T) = μ S + μ T := by
+    sorry
+  let m := hC2.addContent_of_union μ hμ0 hμ3
+  have hm : m.IsSigmaSubadditive := by
+    apply MeasureTheory.isSigmaSubadditive_of_addContent_iUnion_eq_tsum hC2
+    intro f hf1 hf2 hf3
+    -- compact open trick
+    sorry
+  let τ := MeasureTheory.AddContent.measure m hC2.isSetSemiring
+    (BorelSpace.measurable_eq.trans hC5.symm).le hm
+  have hτ1 : τ S0 = 1 := by
+    rwa [MeasureTheory.AddContent.measure_eq m hC2.isSetSemiring
+      (BorelSpace.measurable_eq.trans hC5.symm) hm hS0C]
+  replace hτ1 : τ ≠ 0 := by
+    contrapose! hτ1
+    simp [hτ1]
+  have hτ2 : ∀ g : G, ∀ S : Set X, τ (g • S) = χ g * τ S := by
+    sorry
+  exact ⟨τ, hτ1, hτ2⟩
