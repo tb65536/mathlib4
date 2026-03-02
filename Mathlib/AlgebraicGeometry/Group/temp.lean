a31f2b4af6df6ff5dc47a9a7afbd322392b3c097
tb65536 <thomas.l.browning@gmail.com>
2026-03-02 13:57:16 +0000

    temp

--- Mathlib/AlgebraicGeometry/Group/temp.lean
+++ Mathlib/AlgebraicGeometry/Group/temp.lean
@@ -5,7 +5,9 @@ Authors: Thomas Browning
 -/
 module
 
+public import Mathlib.AlgebraicGeometry.Properties
 public import Mathlib.CategoryTheory.Monoidal.Grp_
+public import Mathlib.CategoryTheory.Monoidal.Internal.Limits
 
 /-!
 
@@ -25,14 +27,57 @@ instance : HasFiniteProducts (Grp (Over X)) := inferInstance
 
 instance : HasFiniteProducts (Grp (Over X)) := inferInstance
 
-instance : HasTerminal (Grp (Over X)) := inferInstance
+instance : HasInitial (Grp (Over X)) := inferInstance
 
-#synth HasInitial (Grp (Over X))
+instance : HasTerminal (Grp (Over X)) := inferInstance
 
 instance : HasZeroObject (Grp (Over X)) := inferInstance
 
-instance : HasZeroMorphisms (Grp (Over X)) := inferInstance
+noncomputable instance : HasZeroMorphisms (Grp (Over X)) := inferInstance
+
+variable {C J : Type*} [Category* C] [SmallCategory J]
+
+instance [SemiCartesianMonoidalCategory C] [HasLimitsOfShape WalkingParallelPair C] :
+    HasKernels (Mon C) :=
+  inferInstance
+
+open MonObj
+
+def tada (G : C) [CartesianMonoidalCategory C] [MonObj G] (h : ∀ (X : C) (f : X ⟶ G), Invertible f) :
+    GrpObj G where
+  inv := Yoneda.fullyFaithful.preimage ⟨fun X f ↦ (h X.unop f).invOf, fun X Y f ↦ by
+    ext g
+    simp only [yoneda_obj_obj, types_comp_apply, yoneda_obj_map]
+    apply invOf_eq_left_inv
+    rw [← comp_mul, invOf_mul_self', comp_one]⟩
+  left_inv := by
+    sorry
+  right_inv := by
+    sorry
+
+noncomputable instance [CartesianMonoidalCategory C] [HasLimitsOfShape J C] :
+    CreatesLimitsOfShape J (Grp.forget₂Mon C) := by
+  constructor
+  intro F
+  let G : Grp C :=
+  { X := (CategoryTheory.Mon.limit (F ⋙ Grp.forget₂Mon C)).X
+    grp := by
+      apply tada
+      intro X f
+      sorry }
+  apply createsLimitOfFullyFaithfulOfIso G
+
+noncomputable instance [CartesianMonoidalCategory C] [HasLimitsOfShape J C] :
+    CreatesLimitsOfShape J (Grp.forget C) :=
+  inferInstanceAs (CreatesLimitsOfShape J (Grp.forget₂Mon C ⋙ Mon.forget C))
+
+instance [CartesianMonoidalCategory C] [HasLimitsOfShape WalkingParallelPair C] :
+    HasKernels (Grp C) := by
+  infer_instance
+  sorry
 
 instance : HasKernels (Grp (Over X)) := inferInstance
+
+
 
 end AlgebraicGeometry
