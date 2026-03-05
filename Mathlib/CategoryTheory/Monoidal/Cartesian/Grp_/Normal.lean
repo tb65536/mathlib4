a538cadfd90c0dd61e012ae3c324f1e41ee4fe62
tb65536 <thomas.l.browning@gmail.com>
2026-03-05 12:20:56 +0000

    merge

--- Mathlib/CategoryTheory/Monoidal/Cartesian/Grp_/Normal.lean
+++ Mathlib/CategoryTheory/Monoidal/Cartesian/Grp_/Normal.lean
@@ -21,16 +21,12 @@ open MonObj MonoidalCategory CartesianMonoidalCategory
 variable {X : Type*} [Category* X] [CartesianMonoidalCategory X] {G H : X} [GrpObj G] [GrpObj H]
   (φ : G ⟶ H) [IsMonHom φ]
 
-def _root_.CategoryTheory.Grp.ofHom : Grp.mk G ⟶ Grp.mk H :=
-  Grp.homMk φ
-
 /-- An `IsMonHom φ : G ⟶ H` is normal if its functor of points has normal ranges. We do not assume
 that `φ` is injective, which would correspond to the additional assumption `[Mono φ]`. -/
 class IsNormalHom : Prop where
   normal : ∀ S : X, ((yonedaGrp.map (Grp.ofHom φ)).app (.op S)).hom.range.Normal
 
-def conj : G ⊗ G ⟶ G :=
-  fst _ _ * snd _ _ * (fst _ _)⁻¹
+#check conj
 
 /- conj `G × G → G` restricted to `G × H` factors through `H`. -/
 
