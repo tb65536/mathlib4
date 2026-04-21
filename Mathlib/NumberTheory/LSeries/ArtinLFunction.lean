00903169299bc486e7f09c704ea5cde867bc84b5
tb65536 <thomas.l.browning@gmail.com>
2026-04-21 11:18:26 +0100

    temp

--- Mathlib/NumberTheory/LSeries/ArtinLFunction.lean
+++ Mathlib/NumberTheory/LSeries/ArtinLFunction.lean
@@ -30,6 +30,22 @@ In this file, we define Artin L-functions.
 
 @[expose] public section
 
+open scoped Pointwise in
+theorem IsArithFrobAt.mem_stabilizer (R : Type*) {S : Type*} [CommRing R] [CommRing S] [Algebra R S]
+    {G : Type*} [Group G] [MulSemiringAction G S] [SMulCommClass G R S] (g : G) (Q : Ideal S)
+    [Q.IsPrime]
+    (h : IsArithFrobAt R g Q) : g ∈ MulAction.stabilizer G Q := by
+  rw [MulAction.mem_stabilizer_iff]
+  conv_lhs => rw [← h.comap_eq]
+  exact Q.map_comap_eq_self_of_equiv (MulSemiringAction.toRingEquiv G S g)
+
+open scoped Pointwise in
+theorem arithFrobAt_mem_stabilizer (R : Type*) {S : Type*} [CommRing R] [CommRing S] [Algebra R S]
+    (G : Type*) [Group G] [MulSemiringAction G S] [SMulCommClass G R S] (Q : Ideal S) [Finite G]
+    [Algebra.IsInvariant R S G] [Q.IsPrime] [Finite (S ⧸ Q)] :
+    arithFrobAt R G Q ∈ MulAction.stabilizer G Q := by
+  apply (IsArithFrobAt.arithFrobAt R G Q).mem_stabilizer R
+
 namespace Representation
 
 instance
@@ -48,16 +64,39 @@ variable (L K : Type*) [Field K] [NumberField K] [Field L] [NumberField L] [Alge
   [AddCommGroup V] [Module k V] [Module.Finite k V]
   (ρ : Representation k G V)
 
+-- todo: restrict from `k` to algebraic integers
+/-- The polynomial associated to a Galois representation. -/
 noncomputable def localPolynomial [Finite G] (p : HeightOneSpectrum (𝓞 K)) : k[X] :=
   let q : p.1.primesOver (𝓞 L) := p.1.nonempty_primesOver.some
-  have : q.1.IsMaximal := sorry
-  have : q.1.LiesOver p.1 := sorry
-  let σ := arithFrobAt (𝓞 K) G q.1
-  let D : Subgroup G := MulAction.stabilizer G q
+  let D : Subgroup G := MulAction.stabilizer G q.1
   let I : Subgroup D := (q.1.inertia G).subgroupOf D
-  let σ' : D := ⟨σ, sorry⟩
+  let σ : G := arithFrobAt (𝓞 K) G q.1
+  have hσ : σ ∈ D := by apply arithFrobAt_mem_stabilizer
+  let σ' : D := ⟨σ, hσ⟩
   let ρ' : Representation k D V := ρ.comp D.subtype
   (ρ'.quotientToCoinvariants I σ').charpoly.reverse -- could drop the quotient, but current formulation makes it easier to prove API
+
+/-- The polynomial associated to a Galois representation. -/
+noncomputable def localPowerSeries [Finite G] (p : HeightOneSpectrum (𝓞 K)) : PowerSeries k :=
+  PowerSeries.invOfUnit (ρ.localPolynomial L K p) 1
+
+/-- The local Euler factor associated to a Weierstrass curve over a nonarchimedean local field. -/
+noncomputable def localEulerFactor [Finite G] (p : HeightOneSpectrum (𝓞 K)) : ArithmeticFunction k :=
+  .ofPowerSeries p.1.absNorm (ρ.localPowerSeries L K p)
+
+/-- The Artin L-function of a representation `ρ` is the product over places of `1 / fₚ(‖p‖⁻ˢ)` where:
+* `fₚ = 1 - aₚ T + ‖p‖ T ^ 2` where `aₚ = ‖p‖ + 1 - |E(K_p)|` if `E` has good reduction at `p`,
+* `fₚ = 1 - T` if `E` has split multiplicative reduction at `p`,
+* `fₚ = 1 + T` if `E` has nonsplit multiplicative reduction at `p`,
+* `fₚ = 1` if `E` has additive reduction at `p`.
+-/
+noncomputable def LFunction : ArithmeticFunction ℤ :=
+  eulerProduct fun p : HeightOneSpectrum (𝓞 K) ↦
+      (W.baseChange (p.adicCompletion K)).localEulerFactor (p.adicCompletionIntegers K)
+
+/-- The L-series of a Weierstrass curve. -/
+protected noncomputable def LSeries (W : WeierstrassCurve K) (s : ℂ) :=
+  LSeries ((↑) ∘ W.LFunction) s
 
 end Representation
 
