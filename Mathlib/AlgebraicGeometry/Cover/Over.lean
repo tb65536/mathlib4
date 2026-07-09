fe5d123f459a004f7aae8dba9c5ca01b08c4734f
tb65536 <thomas.l.browning@gmail.com>
2026-07-09 12:22:42 +0100

    tmp

--- Mathlib/AlgebraicGeometry/Cover/Over.lean
+++ Mathlib/AlgebraicGeometry/Cover/Over.lean
@@ -43,6 +43,7 @@ abbrev Hom.asOverProp {X Y : Scheme.{u}} (f : X.Hom Y) (S : Scheme.{u}) [X.Over 
 
 /-- A `P`-cover of a scheme `X` over `S` is a cover, where the components are over `S` and the
 component maps commute with the structure morphisms. -/
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
 protected class Cover.Over {P : MorphismProperty Scheme.{u}} [P.IsStableUnderBaseChange]
     [IsJointlySurjectivePreserving P] {X : Scheme.{u}} [X.Over S]
     (𝒰 : X.Cover (precoverage P)) where
@@ -54,6 +55,7 @@ attribute [instance] Cover.Over.over Cover.Over.isOver_map
 
 variable [P.IsStableUnderBaseChange] [IsJointlySurjectivePreserving P]
 
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
 instance [P.ContainsIdentities] [P.RespectsIso] {X Y : Scheme.{u}} (f : X ⟶ Y) [X.Over S] [Y.Over S]
     [f.IsOver S] [IsIso f] : (coverOfIsIso (P := P) f).Over S where
   over _ := inferInstanceAs <| X.Over S
@@ -62,15 +64,15 @@ instance [P.ContainsIdentities] [P.RespectsIso] {X Y : Scheme.{u}} (f : X ⟶ Y)
 section
 
 variable {X W : Scheme.{u}} (𝒰 : X.Cover (precoverage P)) (f : W ⟶ X) [W.Over S] [X.Over S]
-  [𝒰.Over S] [f.IsOver S]
+  [f.IsOver S]
 
 set_option backward.isDefEq.respectTransparency false in
 /-- The pullback of a cover of `S`-schemes along a morphism of `S`-schemes. This is not
 definitionally equal to `AlgebraicGeometry.Scheme.Cover.pullback₁`, as here we take
 the pullback in `Over S`, whose underlying scheme is only isomorphic but not equal to the
 pullback in `Scheme`. -/
-@[simps]
-def Cover.pullbackCoverOver : W.Cover (precoverage P) where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+def Cover.pullbackCoverOver [𝒰.Over S] : W.Cover (precoverage P) where
   I₀ := 𝒰.I₀
   X x := (pullback (f.asOver S) ((𝒰.f x).asOver S)).left
   f x := (pullback.fst (f.asOver S) ((𝒰.f x).asOver S)).left
@@ -86,17 +88,19 @@ def Cover.pullbackCoverOver : W.Cover (precoverage P) where
       rw [← Over.forget_map, ← PreservesPullback.iso_hom_fst, P.cancel_left_of_respectsIso]
       exact P.pullback_fst _ _ (𝒰.map_prop j)
 
-instance (j : 𝒰.I₀) : ((𝒰.pullbackCoverOver S f).X j).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance (j : 𝒰.I₀) [𝒰.Over S] : ((𝒰.pullbackCoverOver S f).X j).Over S where
   hom := (pullback (f.asOver S) ((𝒰.f j).asOver S)).hom
 
-instance : (𝒰.pullbackCoverOver S f).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance [𝒰.Over S] : (𝒰.pullbackCoverOver S f).Over S where
   isOver_map j := { comp_over := by exact Over.w (pullback.fst (f.asOver S) ((𝒰.f j).asOver S)) }
 
 set_option backward.isDefEq.respectTransparency false in
 /-- A variant of `AlgebraicGeometry.Scheme.Cover.pullbackCoverOver` with the arguments in the
 fiber products flipped. -/
-@[simps]
-def Cover.pullbackCoverOver' : W.Cover (precoverage P) where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+def Cover.pullbackCoverOver' [𝒰.Over S] : W.Cover (precoverage P) where
   I₀ := 𝒰.I₀
   X x := (pullback ((𝒰.f x).asOver S) (f.asOver S)).left
   f x := (pullback.snd ((𝒰.f x).asOver S) (f.asOver S)).left
@@ -112,24 +116,26 @@ def Cover.pullbackCoverOver' : W.Cover (precoverage P) where
       rw [← Over.forget_map, ← PreservesPullback.iso_hom_snd, P.cancel_left_of_respectsIso]
       exact P.pullback_snd _ _ (𝒰.map_prop j)
 
-instance (j : 𝒰.I₀) : ((𝒰.pullbackCoverOver' S f).X j).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance (j : 𝒰.I₀) [𝒰.Over S] : ((𝒰.pullbackCoverOver' S f).X j).Over S where
   hom := (pullback ((𝒰.f j).asOver S) (f.asOver S)).hom
 
-instance : (𝒰.pullbackCoverOver' S f).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance [𝒰.Over S] : (𝒰.pullbackCoverOver' S f).Over S where
   isOver_map j := { comp_over := by exact Over.w (pullback.snd ((𝒰.f j).asOver S) (f.asOver S)) }
 
 variable {Q : MorphismProperty Scheme.{u}} [Q.HasOfPostcompProperty Q]
   [Q.IsStableUnderBaseChange] [Q.IsStableUnderComposition]
 
-variable (hX : Q (X ↘ S)) (hW : Q (W ↘ S)) (hQ : ∀ j, Q (𝒰.X j ↘ S))
+variable (hX : Q (X ↘ S)) (hW : Q (W ↘ S))
 
 set_option backward.isDefEq.respectTransparency false in
 /-- The pullback of a cover of `S`-schemes with `Q` along a morphism of `S`-schemes. This is not
 definitionally equal to `AlgebraicGeometry.Scheme.Cover.pullbackCover`, as here we take
 the pullback in `Q.Over ⊤ S`, whose underlying scheme is only isomorphic but not equal to the
 pullback in `Scheme`. -/
-@[simps -isSimp]
-def Cover.pullbackCoverOverProp : W.Cover (precoverage P) where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+def Cover.pullbackCoverOverProp [𝒰.Over S] (hQ : ∀ j, Q (𝒰.X j ↘ S)) : W.Cover (precoverage P) where
   I₀ := 𝒰.I₀
   X x := (pullback (f.asOverProp (hX := hW) (hY := hX) S)
     ((𝒰.f x).asOverProp (hX := hQ x) (hY := hX) S)).left
@@ -149,19 +155,23 @@ def Cover.pullbackCoverOverProp : W.Cover (precoverage P) where
       rw [← PreservesPullback.iso_hom_fst, P.cancel_left_of_respectsIso]
       exact P.pullback_fst _ _ (𝒰.map_prop j)
 
-instance (j : 𝒰.I₀) : ((𝒰.pullbackCoverOverProp S f hX hW hQ).X j).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance (j : 𝒰.I₀) [𝒰.Over S] (hQ : ∀ j, Q (𝒰.X j ↘ S)) :
+    ((𝒰.pullbackCoverOverProp S f hX hW hQ).X j).Over S where
   hom := (pullback (f.asOverProp (hX := hW) (hY := hX) S)
     ((𝒰.f j).asOverProp (hX := hQ j) (hY := hX) S)).hom
 
-instance : (𝒰.pullbackCoverOverProp S f hX hW hQ).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance [𝒰.Over S] (hQ : ∀ j, Q (𝒰.X j ↘ S)) : (𝒰.pullbackCoverOverProp S f hX hW hQ).Over S where
   isOver_map j :=
     { comp_over := by exact (pullback.fst (f.asOverProp S) ((𝒰.f j).asOverProp S)).w }
 
 set_option backward.isDefEq.respectTransparency false in
 /-- A variant of `AlgebraicGeometry.Scheme.Cover.pullbackCoverOverProp` with the arguments in the
 fiber products flipped. -/
-@[simps -isSimp]
-def Cover.pullbackCoverOverProp' : W.Cover (precoverage P) where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+def Cover.pullbackCoverOverProp' [𝒰.Over S] (hQ : ∀ j, Q (𝒰.X j ↘ S)) :
+    W.Cover (precoverage P) where
   I₀ := 𝒰.I₀
   X x := (pullback ((𝒰.f x).asOverProp (hX := hQ x) (hY := hX) S)
     (f.asOverProp (hX := hW) (hY := hX) S)).left
@@ -181,11 +191,15 @@ def Cover.pullbackCoverOverProp' : W.Cover (precoverage P) where
       rw [← PreservesPullback.iso_hom_snd, P.cancel_left_of_respectsIso]
       exact P.pullback_snd _ _ (𝒰.map_prop j)
 
-instance (j : 𝒰.I₀) : ((𝒰.pullbackCoverOverProp' S f hX hW hQ).X j).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance (j : 𝒰.I₀) [𝒰.Over S] (hQ : ∀ j, Q (𝒰.X j ↘ S)) :
+    ((𝒰.pullbackCoverOverProp' S f hX hW hQ).X j).Over S where
   hom := (pullback ((𝒰.f j).asOverProp (hX := hQ j) (hY := hX) S)
     (f.asOverProp (hX := hW) (hY := hX) S)).hom
 
-instance : (𝒰.pullbackCoverOverProp' S f hX hW hQ).Over S where
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance [𝒰.Over S] (hQ : ∀ j, Q (𝒰.X j ↘ S)) :
+    (𝒰.pullbackCoverOverProp' S f hX hW hQ).Over S where
   isOver_map j :=
     { comp_over := by exact (pullback.snd ((𝒰.f j).asOverProp S) (f.asOverProp S)).w }
 
@@ -193,12 +207,14 @@ end
 
 variable [P.IsStableUnderComposition]
 variable {X : Scheme.{u}} (𝒰 : X.Cover (precoverage P)) (𝒱 : ∀ x, (𝒰.X x).Cover (precoverage P))
-  [X.Over S] [𝒰.Over S] [∀ x, (𝒱 x).Over S]
+  [X.Over S]
 
-instance (j : (𝒰.bind 𝒱).I₀) : ((𝒰.bind 𝒱).X j).Over S :=
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
+instance (j : (𝒰.bind 𝒱).I₀) [𝒰.Over S] [∀ x, (𝒱 x).Over S] : ((𝒰.bind 𝒱).X j).Over S :=
   inferInstanceAs <| ((𝒱 j.1).X j.2).Over S
 
 set_option backward.defeqAttrib.useBackward true in
+@[deprecated "Use `Cover` in the `Over` category instead." (since := "2026-07-08")]
 instance {X : Scheme.{u}} (𝒰 : X.Cover (precoverage P)) (𝒱 : ∀ x, (𝒰.X x).Cover (precoverage P))
     [X.Over S] [𝒰.Over S] [∀ x, (𝒱 x).Over S] : Cover.Over S (𝒰.bind 𝒱) where
   over := fun ⟨i, j⟩ ↦ inferInstanceAs <| ((𝒱 i).X j).Over S
--- Mathlib/AlgebraicGeometry/Sites/Small.lean
+++ Mathlib/AlgebraicGeometry/Sites/Small.lean
@@ -40,42 +40,48 @@ open CategoryTheory Limits
 namespace AlgebraicGeometry.Scheme
 
 variable {P Q : MorphismProperty Scheme.{u}} {S : Scheme.{u}}
-  [P.IsStableUnderBaseChange]
 
 /-- The presieve defined by a `P`-cover of `S`-schemes. -/
-def Cover.toPresieveOver {X : Over S} (𝒰 : Cover.{u} (precoverage P) X.left) [𝒰.Over S] :
+@[deprecated "Use `𝒰.toPreZeroHypercover.presieve₀` instead. " (since := "2026-07-08")]
+def Cover.toPresieveOver {X : Over S}
+    (𝒰 : Precoverage.ZeroHypercover.{u} ((precoverage P).comap (Over.forget S)) X) :
     Presieve X :=
-  Presieve.ofArrows (fun i ↦ (𝒰.X i).asOver S) (fun i ↦ (𝒰.f i).asOver S)
+  𝒰.toPreZeroHypercover.presieve₀
 
 /-- The presieve defined by a `P`-cover of `S`-schemes with `Q`. -/
-def Cover.toPresieveOverProp {X : Q.Over ⊤ S} (𝒰 : Cover.{u} (precoverage P) X.left) [𝒰.Over S]
-    (h : ∀ j, Q (𝒰.X j ↘ S)) : Presieve X :=
-  Presieve.ofArrows (fun i ↦ (𝒰.X i).asOverProp S (h i)) (fun i ↦ (𝒰.f i).asOverProp S)
+def Cover.toPresieveOverProp {X : Q.Over ⊤ S}
+    (𝒰 : Precoverage.ZeroHypercover.{u}
+      (((precoverage P).comap (Over.forget S)).comap (MorphismProperty.Over.forget Q ⊤ S)) X) :
+    Presieve X :=
+  𝒰.toPreZeroHypercover.presieve₀
 
 set_option backward.defeqAttrib.useBackward true in
 lemma Cover.overEquiv_generate_toPresieveOver_eq_ofArrows {X : Over S}
-    (𝒰 : Cover.{u} (precoverage P) X.left)
-    [𝒰.Over S] : Sieve.overEquiv X (Sieve.generate 𝒰.toPresieveOver) =
-      Sieve.ofArrows 𝒰.X 𝒰.f := by
+    (𝒰 : Precoverage.ZeroHypercover.{u} ((precoverage P).comap (Over.forget S)) X) :
+    Sieve.overEquiv X (Sieve.generate 𝒰.toPreZeroHypercover.presieve₀) =
+      Sieve.ofArrows (fun i ↦ (𝒰.X i).left) (fun i ↦ (𝒰.f i).left) := by
   ext V f
   simp only [Sieve.overEquiv_iff, Sieve.generate_apply]
   constructor
   · rintro ⟨U, h, g, ⟨k⟩, hcomp⟩
-    exact ⟨𝒰.X k, h.left, 𝒰.f k, ⟨k⟩, congrArg CommaMorphism.left hcomp⟩
+    exact ⟨(𝒰.X k).left, h.left, (𝒰.f k).left, ⟨k⟩, congrArg CommaMorphism.left hcomp⟩
   · rintro ⟨U, h, g, ⟨k⟩, hcomp⟩
-    have : 𝒰.f k ≫ X.hom = 𝒰.X k ↘ S := comp_over (𝒰.f k) S
-    refine ⟨(𝒰.X k).asOver S, Over.homMk h (by simp [← hcomp, this]), (𝒰.f k).asOver S, ⟨k⟩, ?_⟩
+    have : (𝒰.f k).left ≫ X.hom = (𝒰.X k).hom := (𝒰.f k).w
+    refine ⟨𝒰.X k, Over.homMk h (by simp [← hcomp, this]), 𝒰.f k, ⟨k⟩, ?_⟩
     ext : 1
     simpa
 
 lemma Cover.toPresieveOver_le_arrows_iff {X : Over S} (R : Sieve X)
-    (𝒰 : Cover.{u} (precoverage P) X.left) [𝒰.Over S] :
-    𝒰.toPresieveOver ≤ R.arrows ↔
-      Presieve.ofArrows 𝒰.X 𝒰.f ≤ (Sieve.overEquiv X R).arrows := by
+    (𝒰 : Precoverage.ZeroHypercover.{u} ((precoverage P).comap (Over.forget S)) X) :
+    𝒰.toPreZeroHypercover.presieve₀ ≤ R.arrows ↔
+      Presieve.ofArrows (fun i ↦ (𝒰.X i).left) (fun i ↦ (𝒰.f i).left) ≤
+        (Sieve.overEquiv X R).arrows := by
   simp_rw [← Sieve.giGenerate.gc.le_iff_le, ← (Sieve.overEquiv X).map_rel_iff]
   rw [overEquiv_generate_toPresieveOver_eq_ofArrows]
 
 variable [P.IsMultiplicative] [P.RespectsIso]
+
+variable [P.IsStableUnderBaseChange]
 
 variable (P Q S)
 
